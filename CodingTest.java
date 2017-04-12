c1c493b83ad58d794c11579894016fb2d4843825
Harnoor Powar <hsp2112@columbia.edu>
2017-04-12 18:21:11 -0400

    Update CodingTest.java

--- CodingTest.java
+++ CodingTest.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ Solution To Frequent Numbers in Window Challenge
  */
 package codingtest;
 import java.util.*;
